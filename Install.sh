098c2f7d61b88012fe5ac88d7d55a952a50f3e58
Lilith <44578053+LilithRainbows@users.noreply.github.com>
2022-04-03 06:35:40 -0300

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ARCH=`dpkg --print-architecture`
+if [ $ARCH != 'amd64' ]; then
+	echo "Wrong architecture, only x64 is supported!"
+    exit
+fi
+
+echo "[Checking dependencies]"
+pkgs='unzip wget'
+install=false
+for pkg in $pkgs; do
+    status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
+    if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
+        install=true
+        break
+    fi
+done
+if "$install"; then
+    sudo apt install $pkgs --assume-yes
+fi  
+
+HabboAirForLinuxAppPath=~/.local/share/applications/HabboAirForLinux
+rm -r $HabboAirForLinuxAppPath
+mkdir -p $HabboAirForLinuxAppPath
+wget https://github.com/LilithRainbows/HabboAirForLinux/raw/main/HabboAirLinuxPatch.zip -O $HabboAirForLinuxAppPath/HabboAirLinuxPatch.zip
+wget https://github.com/LilithRainbows/HabboAirForLinux/raw/main/HabboLauncher.sh -O $HabboAirForLinuxAppPath/HabboLauncher.sh
+wget https://github.com/LilithRainbows/HabboAirForLinux/raw/main/HabboAirForLinux.png -O ~/.icons/HabboAirForLinux.png
+wget https://github.com/LilithRainbows/HabboAirForLinux/raw/main/HabboAirForLinux.desktop -O $HabboAirForLinuxAppPath/../HabboAirForLinux.desktop
+chmod +x $HabboAirForLinuxAppPath/HabboLauncher.sh
+xdg-settings set default-url-scheme-handler habbo HabboAirForLinux.desktop
+
+echo "[Installation finished]"
+echo "Now you can use the Habbo Launch button from the web."
